5ccf64662e2d98750c3544f0096e06ffaecc92ee
Sebastian Becker <mail@sbecker.de.com>
2026-03-17 18:57:15 +0100

    chore: upgrade vitest and ignore extension build output

--- controller-extension/out/schema.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * Type definitions for the Adagio Agent REST API.
- * These match the contract described in the architecture spec.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=schema.js.map
